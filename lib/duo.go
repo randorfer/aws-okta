27b2fca4415f91f450cc6d36b6000a5ba48da932
Ryan Andorfer <ryan.andorfer@tanium.com>
2019-08-16 12:54:37 -0500

    logging

--- lib/duo.go
+++ lib/duo.go
@@ -451,7 +451,7 @@ func (d *DuoClient) DoStatus(txid, sid string) (auth string, status StatusResp, 
 		return
 	}
 	defer res.Body.Close()
-	log.Printf("%+v\n", res)
+	log.Printf("res: %+v\n", res)
 	if res.StatusCode != http.StatusOK {
 		err = fmt.Errorf("Prompt request failed: %d", res.StatusCode)
 		return
@@ -459,6 +459,7 @@ func (d *DuoClient) DoStatus(txid, sid string) (auth string, status StatusResp, 
 
 	err = json.NewDecoder(res.Body).Decode(&status)
 
+	log.Printf("Status: %+v\n", status)
 	if status.Response.Result == "SUCCESS" {
 		if status.Response.ResultURL != "" {
 			auth, err = d.DoRedirect(status.Response.ResultURL, sid)
